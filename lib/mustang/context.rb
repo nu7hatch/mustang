5f6ba96d31815b4cf32dcd50f615d4fd2810adfb
Chris Kowalik <chris@nu7hat.ch>
2011-03-27 15:30:01 -0300

    added files loading to context

--- lib/mustang/context.rb
+++ lib/mustang/context.rb
@@ -1,10 +1,33 @@
 module Mustang
+  # Raised when file specified to load doesn't exist. 
+  class ScriptNotFoundError < Errno::ENOENT 
+  end
+
+  # Extended and more user-friendly version of <tt>Mustang::V8::Context</tt>.
   class Context < V8::Context
-    
+    # Evaluates given javascript source. Before evaluation it's able to
+    # set given local variables within current context, eg:
+    #
+    #   rt = Mustang::Context.new
+    #   rt.evaluate("foo=1")                     # => 1
+    #   rt.evaluate("bar=foo+spam", :spam => 10) # => 11
+    #   rt.evaliate("bar")                       # => 11
+    #
     def evaluate(source, locals={}, filename="<eval>")
       super(source, filename)
     end
     alias_method :eval, :evaluate
 
+    # Loads and evaluates given list of javascript files, eg:
+    #
+    #   rt = Mustang::Runtime.new
+    #   rt.load("foo.js", "bar.js")
+    #
+    def load(*files)
+      files.each { |filename|
+        raise ScriptNotFoundError, "Script `#{filename}' does not exts." unless File.exists?(filename)
+        evaluate(File.read(filename), {}, filename)
+      }
+    end
   end # Context
 end # Mustang
